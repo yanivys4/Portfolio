547862397e1eb923272357b3cbadc4ac0bf368df
yanivys4 <yanivys4@gmail.com>
2021-09-17 00:37:35 +0300

    add projects and contact sections

--- public/JS/script.js
+++ public/JS/script.js
@@ -11,5 +11,27 @@ jQuery(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    // owl carousel script
+    $('.owl-carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        center:true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items: 1,
+                
+            },
+            600:{
+                items: 2,
+                
+            },
+            1000:{
+                items: 3,
+            }
+        }
+    });
 });
 
