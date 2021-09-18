66caa314594eb46dd0161827eb33c6f12b713a4d
yanivys4 <yanivys4@gmail.com>
2021-09-18 13:04:43 +0300

    Added final animations

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,17 +1,34 @@
-jQuery(document).ready(function(){
-    $(window).scroll(function(){
+jQuery(function(){
+    $(window).on("scroll",function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').on("click",function(){
+        $('html').animate({scrollTop: 0});
     });
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').on("click",function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
+    // typing animetion script
+    var typed = new Typed(".typing",{
+        strings: ["Software developer","Software Engineer","Computer Scientist","Team player"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
     // owl carousel script
     $('.owl-carousel').owlCarousel({
         margin:20,
