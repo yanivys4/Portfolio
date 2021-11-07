79f820b0e2985add9881769ffdffd67fcaed3fe0
yanivys4 <yanivys4@gmail.com>
2021-11-07 22:10:56 +0200

    .

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
 const app = express();
-app.use(cors());
 const path = require('path');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
