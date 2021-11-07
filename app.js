9e9913e7810f6df70110effdf38b94ba010742a6
yanivys4 <yanivys4@gmail.com>
2021-11-07 22:09:17 +0200

    .

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
 app.use(cors());
 const path = require('path');
